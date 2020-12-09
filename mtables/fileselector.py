f71df699d6cc1d2791aa26ad99eb4d838c7e8f0d
phaggi <phaggi@gmail.com>
2020-12-09 22:19:26 +0300

    change return of makefilelist, add Path, add makefiledict

--- mtables/fileselector.py
+++ mtables/fileselector.py
@@ -1,11 +1,18 @@
 import os
 from pprint import pprint
 from tkinter import Tk, filedialog
+from pathlib import Path
 
 from configs import config
 
+file_types = config.file_types
+blacklist = config.blacklist
+whitelist = config.whitelist
+numfil = config.numfil
+
 
 def walk_level(some_dir):
+    some_dir = Path(some_dir)
     file_list = set()
     with os.scandir(some_dir) as it:
         for entry in it:
@@ -46,9 +53,10 @@ def select_folder():
     return filedialog.askdirectory()
 
 
-def make_file_list(file_types: list = config.file_types,
-                   blacklist: list = config.blacklist,
-                   whitelist: list = config.whitelist):
+def make_file_list(file_types: list,
+                   blacklist: list,
+                   whitelist: list,
+                   folder_selected: str = ''):
     """
 
     :param whitelist: whitelist of good words (str)
@@ -56,17 +64,39 @@ def make_file_list(file_types: list = config.file_types,
     :param file_types: list of types of files (str)
     :return: tuple: folder, sorted list of files in folder, with type in file_types and without blacklist in name
     """
-    folder_selected = select_folder()
+    if not folder_selected:
+        folder_selected = select_folder()
     file_list = set()
     for filename in walk_level(folder_selected):
         filename_parts = filename.split('.')
         if len(filename_parts) > 1:
             file_type = filename_parts[len(filename_parts) - 1]
-            if (file_type in file_types) and test_blacklist(filename, blacklist) and test_whitelist(filename, whitelist):
-                file_list.update({filename})
-    return folder_selected, sorted(list(file_list))
+            if (file_type in file_types) and test_blacklist(filename, blacklist) and test_whitelist(filename,
+                                                                                                    whitelist):
+                file_list.update({folder_selected/filename})
+    return sorted(list(file_list))
+
+
+def make_file_dict(file_list: list, num_fil: dict):
+    file_dict = {}
+    for file in file_list:
+        is_number = False
+        number = None
+        for key in num_fil.keys():
+            if key in str(file):
+                is_number = True
+                number = num_fil[key]
+        if is_number:
+            file_dict.update({number: file})
+    return file_dict
 
 
 if __name__ == '__main__':
-    pprint(make_file_list())
     print(test_whitelist('test', ['test']))
+    pprint(f'walk: {walk_level("/Users/phaggi/Documents/_test")}')
+    my_file_list = make_file_list(file_types=file_types,
+                                  blacklist=blacklist,
+                                  whitelist=whitelist,
+                                  folder_selected=Path('/Users/phaggi/Documents/_test'))
+    pprint(f'file_list:{my_file_list}')
+    pprint(make_file_dict(file_list=my_file_list, num_fil=numfil))
