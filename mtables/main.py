d501ce37fe2021a57d70dbb145293045c8d129e3
phaggi <phaggi@gmail.com>
2020-12-09 20:52:27 +0300

    change some temporary tests

--- mtables/main.py
+++ mtables/main.py
@@ -1,5 +1,11 @@
 from pprint import pprint
+
+from configs import config
 from mtables import fileselector
+from mtables import sheet_finder
 
 if __name__ == '__main__':
-    pprint(fileselector.make_file_list())
+    folder, filenames = fileselector.make_file_list()
+    pprint(filenames)
+    for file in filenames:
+        print(f'finded: {sheet_finder.getsheetname("/".join([folder, file]), ["Лист 1"])}')
