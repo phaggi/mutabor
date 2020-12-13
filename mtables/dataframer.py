670441d3920cdb18b8932f93a1af82988cb70e1d
phaggi <phaggi@gmail.com>
2020-12-13 14:44:50 +0300

    add write result and some tests, update filter_df and concat_df

--- mtables/dataframer.py
+++ mtables/dataframer.py
@@ -1,11 +1,9 @@
-from pathlib import Path
-
-from mtables import fileselector
-from mtables import sheet_finder
-from configs import config
 import pandas as pd
 import numpy as np
-from mtables.sheet_finder import getsheetname
+from pathlib import Path
+from pprint import pprint
+from mtables import fileselector
+from configs import config
 
 pd.set_option('use_inf_as_na', True)
 
@@ -35,32 +33,55 @@ def get_df(filename, sheetname, word_in_header, _header=None):
     if not _header:
         _header = 1
     df = pd.read_excel(filename, sheet_name=sheetname, header=_header)
+    df.replace(np.nan, '-', inplace=True)
     df.fillna(value='-', inplace=True)
     return df
 
 
-'''def make_df(file_dict: dict, fil_numb, sheetnames, column_name, header=None):
-    if not header:
-        _header = 1
-    dfprev = False
-    for key in file_dict.keys():
-        file = file_dict[key]
-        sheet_name = getsheetname(full_filename=file, sheet_name_elements=sheetnames)
-        print(sheet_name)
+def filter_df(df: pd.DataFrame, column_name: str, search_for: str):
+    return df[df[column_name].str.contains(search_for, na=False)]
+
+
+def concat_df(file_dict: dict, sheetname: str, columnname: str, fil_numb: dict, header: int):
+    df_prev = False
+    df_now = False
+    for key, filename in file_dict.items():
+        next_df = get_df(filename=filename, sheetname=sheetname, word_in_header=columnname, _header=header)
         search_for = '|'.join(fil_numb[key])
-        my_df = pd.read_excel(file, sheet_name=sheet_name)  # , use_inf_as_na=False)
-        print(type(my_df))
-        my_df.replace(np.nan, 0, inplace=True)
-        my_df.fillna(0, inplace=True)
-
-        print(search_for)
-        if isinstance(dfprev, bool):
-            dfprev = my_df[my_df[column_name].str.contains(search_for, na=False)]
+        if isinstance(df_prev, bool):
+            df_prev = filter_df(df=next_df, column_name=columnname, search_for=search_for)
         else:
-            dfnow = my_df[my_df[column_name].str.contains(search_for, na=False)]
-        dfprev = pd.concat([dfprev, dfnow], sort=False)
-    return dfprev'''
+            df_now = filter_df(df=next_df, column_name=columnname, search_for=search_for)
+            df_prev = pd.concat([df_prev, df_now], sort=False)
+    return df_prev
 
+
+def write_result(df: pd.DataFrame,
+                 folder_selected: Path,
+                 result_file_name: str,
+                 result_file_type: str,
+                 set_data: bool = None):
+    if not set_data:
+        set_data = False
+
+    if set_data:
+        data = str('2020.12.12')
+        resultfilename = '.'.join([result_file_name, data.strip(), result_file_type])
+    else:
+        data = ''
+        resultfilename = '.'.join([result_file_name, result_file_type])
+    print(resultfilename)
+    out_file_name = folder_selected/resultfilename
+    print(out_file_name)
+    try:
+        xlWriter = pd.ExcelWriter(out_file_name, engine='xlsxwriter')
+        workbook = xlWriter.book
+        df.to_excel(xlWriter, encoding='utf-8', index=False, sheet_name='сборка')
+        xlWriter.save()
+        xlWriter.close()
+        print('Файл ', out_file_name, ' сохранен')
+    except IOError:
+        print("Could not open file! Please close Excel!")
 
 if __name__ == '__main__':
     numfil = config.numfil
@@ -69,10 +90,18 @@ if __name__ == '__main__':
     whitelist = config.whitelist
     filnumb = config.filnumb
     sheet_names = config.sheet_name_elements
-    my_folder = Path('/Users/phaggi/Documents/_test')
+    names = ['Валеев', 'Петрова', 'Конахин']
+    my_folder = Path('/Users/phaggi/PycharmProjects/mutabor/tests/данные от филиалов/Гермес')
     filelist, folder = fileselector.make_file_list(file_types=filetypes, blacklist=blacklist, whitelist=whitelist,
                                                    folder=my_folder)
     filedict = fileselector.make_file_dict(file_list=filelist, num_fil=numfil)
-    my_df = get_df(filename=Path('../tests/test.xlsx'), sheetname='testname', word_in_header='ИТОГО')
-    # xl_df = make_df(file_dict=filedict, fil_numb=filnumb, sheetnames=['Лист 1'], column_name='ИТОГО')
-    print(my_df.head())
+    pprint(filedict)
+    myfile = list(filedict.items())[0][1]
+    print(myfile)
+
+    key = numfil['БО']
+    search_for = '|'.join(filnumb[key])
+    # my_df = get_df(filename=myfile, sheetname='Гермес свод', word_in_header='Филиал', _header=7)
+    # filtered_df = filter_df(df=my_df, column_name='Филиал', search_for=search_for)
+    result_df = concat_df(file_dict=filedict, sheetname='Гермес свод', columnname='Филиал', fil_numb=filnumb, header=7)
+    write_result(result_df, folder, config.result_filename, config.result_filetype, set_data=True)
